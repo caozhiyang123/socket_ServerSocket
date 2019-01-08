c69a13169903817643f84fb799dfc68d882abc48
caozhiyang123 <1250233433@qq.com>
2019-01-08 19:37:59 +0800

    Revert "del leaving code"
    This reverts commit 59774950dbd919a6fec187dc7df97e36b778f1a9.

--- LoginClient/src/com/baifenjy/frame/ManagerFrame.java
+++ LoginClient/src/com/baifenjy/frame/ManagerFrame.java
@@ -59,11 +59,28 @@ public class ManagerFrame extends JFrame{
         try {
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
             
-          //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceBusinessLookAndFeel");
-            //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeSilver2007LookAndFeel");
-            //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceSaharaLookAndFeel");
-            //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeBlue2007LookAndFeel");
-            //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel");
+            java.awt.EventQueue.invokeLater(new Runnable()
+            {
+                
+                @Override
+                public void run()
+                {
+                    try
+                    {
+                        //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceBusinessLookAndFeel");
+                        //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeSilver2007LookAndFeel");
+                        //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceSaharaLookAndFeel");
+                        //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceOfficeBlue2007LookAndFeel");
+                        //UIManager.setLookAndFeel("org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel");
+                        //UIManager.setLookAndFeel(SubstanceLookAndFeel.SKIN_PROPERTY);
+                        
+                    } catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    } 
+                    
+                }
+            });
           
           new ManagerFrame();
         } catch (Exception e) {
